b8cf80a54fa2b844888da8913f6b6346762d8d99
Anderson Martins <atmmoreira.rj@gmail.com>
2022-11-22 22:03:59 -0300

    feature: new project nodejs-prompt

---
+++ discipline.node/nodejs-prompt/index.js
@@ -0,0 +1,38 @@
+const prompt = require("prompt");
+const colors = require("@colors/colors/safe");
+
+const schema = {
+  properties: {
+    name: {
+      description: colors.magenta("Informe seu nome?"),
+      message: "Utilize apenas letras",
+      pattern: /^[a-zA-Z\s\-]+$/,
+      required: true,
+    },
+    lastname: {
+      description: colors.blue("Informe seu sobrenome?"),
+      message: "Utilize apenas letras",
+      pattern: /^[a-zA-Z\s\-]+$/,
+      required: true,
+    },
+    email: {
+      description: colors.green("Informe seu email?"),
+      message: "Preencha um email válido",
+      required: true,
+    },
+    cpf: {
+      description: colors.yellow("Informe seu CPF?"),
+      message: "Preencha um CPF válido",
+      required: true,
+    },
+  },
+};
+prompt.start();
+
+prompt.get(schema, function (err, result) {
+  console.log("Recebendo informações:");
+  console.log("  Nome: " + result.name);
+  console.log("  Sobrenome: " + result.lastname);
+  console.log("  E-mail: " + result.email);
+  console.log("  CPF: " + result.cpf);
+});
